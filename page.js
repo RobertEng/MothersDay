818a4c21e473d0852044d321a7faaee8fc164e51
RobertEng <googlypantsroberteng@gmail.com>
2015-05-09 16:24:26 -0700

    Added a nice clock whic updates every second

--- page.js
+++ page.js
@@ -39,7 +39,55 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	document.getElementById("date").innerHTML = disstr;
 
+
+	// Add the time when tab opens
+	var currentTime = new Date ();
+	var currentHours = currentTime.getHours();
+	var currentMinutes = currentTime.getMinutes();
+	var currentSeconds = currentTime.getSeconds();
+	// Pad the minutes and seconds with leading zeros, if required
+	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+	// Choose either "AM" or "PM" as appropriate
+	var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+	// Convert the hours component to 12-hour format if needed
+	currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+	// Convert an hours component of "0" to "12"
+	currentHours = (currentHours == 0) ? 12 : currentHours;
+	// Compose the string for display
+	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+	// Update the time display
+	document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+
+
+
+	// Poop chrome extensions don't allow for javascript functions in setInterval.
+	// The function in setInterval is same as above.
+	// This setInterval updates every second
+	setInterval(function() {
+		var currentTime = new Date ();
+		var currentHours = currentTime.getHours();
+		var currentMinutes = currentTime.getMinutes();
+		var currentSeconds = currentTime.getSeconds();
+		// Pad the minutes and seconds with leading zeros, if required
+		currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+		currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+		// Choose either "AM" or "PM" as appropriate
+		var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+		// Convert the hours component to 12-hour format if needed
+		currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+		// Convert an hours component of "0" to "12"
+		currentHours = (currentHours == 0) ? 12 : currentHours;
+		// Compose the string for display
+		var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+		// Update the time display
+		document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+	}, 1000);
+
+
 });
+
+// This function is from online somewhere
 
 
 
